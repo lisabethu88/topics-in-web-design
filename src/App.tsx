315510203fee83d6bb8d5cab833e091688b813e9
Lisa Utsett <lisabethutsett@gmail.com>
2026-03-08 12:03:44 -0400

    Modifies existin App and CSS files

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,17 @@
 import "./App.css";
-import NavigationHeader from "./components/NavigationHeader";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
 
 function App() {
   return (
-    <>
-      <NavigationHeader />
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
-    </>
+    <Router>
+      <Routes>
+        {/* Routes with layout */}
+        <Route element={<Layout />}>
+          <Route path="/" element={<></>} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
