8f4ab8884998e0e360b8d35e5eb915c6630d3561
Lisa Utsett <lisabethutsett@gmail.com>
2026-03-08 12:06:56 -0400

    Adds a style folder for organizing style constants meant for modifying MUI components

---
+++ src/styles/colors.ts
@@ -0,0 +1,2 @@
+export const lightYellow = "#fafaf7"; // background color
+export const darkGray = "#201f1f"; // main font color
---
+++ src/styles/styles.ts
@@ -0,0 +1,17 @@
+import { darkGray } from "./colors";
+
+export const navItemStyle = {
+  fontFamily: "'Alata', sans-serif",
+  textTransform: "capitalize",
+  fontWeight: 400,
+  fontStyle: "normal",
+  color: darkGray,
+};
+
+export const logoStyle = {
+  fontFamily: "'Raleway', sans-serif",
+  fontWeight: 800,
+  fontStyle: "normal",
+  letterSpacing: 8,
+  color: darkGray,
+};
